a52491a11a563bb1f4686575474305fb7853140c
zhanglm <langmin_zhang@163.com>
2018-12-12 16:07:47 +0800

    1

--- src/main/java/com/frame/util/MainConfig.java
+++ src/main/java/com/frame/util/MainConfig.java
@@ -1,8 +1,15 @@
 package com.frame.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaAdmin;
 
 /**
  * @author zhanglm
@@ -19,4 +26,22 @@ public class MainConfig {
 //	    return messageSource;
 //	}
 	
+//	@Configuration
+//	@EnableKafka
+//	public class KafkaConfig {
+//	//topic config Topic的配置开始
+//	    @Bean
+//	    public KafkaAdmin admin() {
+//	        Map<String, Object> configs = new HashMap<String, Object>();
+//	        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.180.128:9092");
+//	        return new KafkaAdmin(configs);
+//	    }
+//
+//	    @Bean
+//	    public NewTopic topic1() {
+//	        return new NewTopic("foo", 10, (short) 2);
+//	    }
+//	//topic的配置结束
+//
+//	}
 }
--- src/main/java/com/frame/web/FrameApplication.java
+++ src/main/java/com/frame/web/FrameApplication.java
@@ -1,18 +1,22 @@
 package com.frame.web;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.core.KafkaTemplate;
 
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
 @MapperScan("com.frame.dao")
 @ComponentScan(basePackages = {"com.frame"})
-public class FrameApplication {
+public class FrameApplication implements CommandLineRunner{
 
 	
 	public static void main(String[] args) {	
@@ -20,4 +24,19 @@ public class FrameApplication {
 		SpringApplication.run(FrameApplication.class, args);
 		log.info("spring初始化结束");
 	}
+	
+	
+	 @Autowired
+	   private KafkaTemplate<String, String> template;
+	 
+	   private final CountDownLatch latch = new CountDownLatch(4);
+	 
+	   @Override
+	   public void run(String... args) throws Exception {
+	      this.template.send("myTopic", "foo1");
+	      this.template.send("myTopic", "foo2");
+	      this.template.send("myTopic", "foo3");
+	      this.template.send("myTopic", "hi", "foo4");
+	      this.template.send("myTopic2", "2", "foo5");
+	   }
 }
